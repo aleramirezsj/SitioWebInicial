e4f62ab8613bcfc68508c1f57e26b9d5158c002e
aleramirezsj <aleramirezsj@gmail.com>
2022-06-15 16:40:12 -0300

    inicio del proyecto

---
+++ js/codigo.js
@@ -0,0 +1,17 @@
+function preguntarNombreYApellido(){
+    var nombre=prompt("Ingresa tu nombre:");
+    var apellido=prompt("Ingresa tu apellido:");
+    alert("Hola "+nombre+" "+apellido);
+    alert(`Hola tu nombre es ${nombre} y tu apellido es ${apellido}`);
+}
+function ponerEnMayusculas(){
+    var datosPersonales=document.getElementById("datos");
+    datosPersonales.innerHTML="DATOS PERSONALES";
+    datosPersonales.style.background="white";
+}
+
+function AgregarFila(){
+    var tabla=document.getElementById("tabla");
+    var nuevaFila='<tr><td>Ramirez</td><td>Rubén Alejandro</td><td>19</td><td>07</td><td>1976</td><td >Fco Romero 1823</td></tr>';
+    tabla.innerHTML=tabla.innerHTML+nuevaFila;
+}
